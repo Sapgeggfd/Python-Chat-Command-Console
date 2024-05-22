beaa091c93e2356c0075f449e0846e4f8783134a
Simon Geigenfeind <sgeigenfeind@cms-it.de>
2024-05-22 10:14:27 +0200

    modified player.py

--- player.py
+++ player.py
@@ -1,10 +1,31 @@
 import uuid
+import item
 
 
 class Player:
     id: uuid.UUID
     name: str
+    _pos: tuple[int, int]
+    inventory: dict[object, int]
+
+    @property
+    def pos(self) -> tuple[int, int]:
+        return self.pos
+
+    @pos.setter
+    def pos(self, value: tuple[int, int]) -> None:
+        self._pos = value
+        print(f"new_pos: {self._pos}")
 
     def __init__(self, name: str) -> None:
         self.id = uuid.uuid4()
         self.name = name
+        self._pos = (0, 0)
+        self.inventory = {}
+
+    def add_item(self, item: item.Item) -> None:
+        print(f"{self.name} received {item.itemID}")
+        if item in self.inventory:
+            self.inventory[item] += 1
+        else:
+            self.inventory[item] = 1
