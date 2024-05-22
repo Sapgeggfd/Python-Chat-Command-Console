19f1ec7b68de959519912c97a5b8b443ec82452a
Simon Geigenfeind <sgeigenfeind@cms-it.de>
2024-05-22 16:04:12 +0200

    updated Helper protocols

--- helper_protocols.py
+++ helper_protocols.py
@@ -1,4 +1,15 @@
 from typing import Protocol
 
 
-class Player(Protocol): ...
+class Item(Protocol):
+    itemID: str
+
+
+class Player(Protocol):
+    _pos: tuple[int, int]
+
+    @property
+    def pos(self) -> tuple[int, int]: ...
+    @pos.setter
+    def pos(self, value: tuple[int, int]) -> None: ...
+    def add_item(self, item: Item): ...
