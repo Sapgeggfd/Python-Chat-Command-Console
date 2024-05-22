f1eba4ec934350c4ebf2f239944798b9b155069d
Simon Geigenfeind <sgeigenfeind@cms-it.de>
2024-05-22 17:08:44 +0200

    updated workflow

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-# Read the contents of your README file
-from pathlib import Path
+import os
 
 from setuptools import find_packages, setup
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+# Get the version from the environment variable
+version = os.getenv("VERSION", "0.0.0")
 
 setup(
-    name="pygchat",  # This is the name of your package
-    version="0.1.0",  # Update this for new versions
-    author="Sapge",
-    author_email="sapge.ggfd@gmail.com",
-    description="A Chat System to Use in Python games",
-    long_description=long_description,
+    name="pygchat",
+    version=version,
+    description="Your package description",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/your-repo",  # Replace with your GitHub repo URL
-    packages=find_packages(),  # Automatically find your packages
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/your-repo",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Creative Commons Attribution 4.0 International License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=[],
-    entry_points={  # If your package has command-line scripts
-        "console_scripts": [],
-    },
+    install_requires=[
+        # Your package dependencies
+    ],
 )
