016fe1032543b81e595b9ad490627d3ae0284546
Simon Ggfd <simon.ggfd@web.de>
2024-05-24 15:33:23 +0200

    renamed kwarg

--- examples/chat_system.py
+++ examples/chat_system.py
@@ -1,8 +1,9 @@
 from player import Player
+
 from pygchat.chat import Chat
 
 player1 = Player(name="Player1")
-chat = Chat(player=player1)
+chat = Chat(issuer=player1)
 
 
 def insert_text_to_chat(txt: str) -> None:
